14b9d10e711dfd5ea10db87aed9fe71badefef26
Tom Nabarro <tom.nabarro@intel.com>
2016-05-13 16:01:08 +0100

    auto update

--- git_auto.bat
+++ git_auto.bat
@@ -1,7 +1,8 @@
+:: Obsolete, now uses cron job on linux vm with shared drives instead
 :: batch wrapper to push git repositories to online gitub
-C:
-chdir C:\cygwin64\bin
+::C:
+::chdir C:\cygwin64\bin
 ::bash --login C:\Users\tan.000\git_auto_push.sh
-bash --login /home/tanabarr/src/scripts/git_auto_push.sh
+::bash --login /home/tanabarr/src/scripts/git_auto_push.sh
 ::bash --login /home/TAN/src/scripts/vim_bundle_ovwrite.sh
 ::pause
