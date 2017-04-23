38e0ff37205ba886149a2156fd2aae519efbcad1
Tom Nabarro <tom.nabarro@intel.com>
2017-04-23 12:00:06 +0100

    auto update

--- mount_first_VM.bat
+++ mount_first_VM.bat
@@ -1,7 +1,7 @@
 :: starts VM then mounts share on VM
-cd "c:\Program Files\Oracle\VirtualBox"
+::cd "c:\Program Files\Oracle\VirtualBox"
 :: command implicitly waits for vm to load
-vboxmanage.exe startvm "Centos67imldev" --type headless
+::vboxmanage.exe startvm "Centos67imldev" --type headless
 umount g:
 choice /c yn /t 8 /d y >NUL		
 ::mount -u:tanabarr -p:"Chanch0306!" \\192.168.56.102\home\share g:
@@ -9,12 +9,12 @@ mount -o fileaccess=644 \\192.168.56.101\home\share g:
 choice /c yn /t 4 /d y >NUL		
 
 :: now we have mount loaded, pycharm should also load project chroma
-start "" "C:\Program Files (x86)\JetBrains\PyCharm 4.5.4\bin\pycharm.exe"
-choice /c yn /t 2 /d y >NUL		
+::start "" "C:\Program Files (x86)\JetBrains\PyCharm 4.5.4\bin\pycharm.exe"
+::choice /c yn /t 2 /d y >NUL		
 
 :: open terminal to vm
-start "" "C:\win scripts\first VM.bat"
-choice /c yn /t 2 /d y >NUL		
+::start "" "C:\win scripts\first VM.bat"
+::choice /c yn /t 2 /d y >NUL		
 
 exit
 
--- view_first_VM.bat
+++ view_first_VM.bat
@@ -1,8 +1,7 @@
 @echo off
 cd "C:\win scripts" 
-start_first_VM.bat
-mount_first_VM.bat
-view_first_VM.bat
+start putty.exe -load "first VM"
+start putty.exe -load "lotus-32vm2"
 
 :: now we have mount loaded, pycharm should also load project chroma
 ::start "" "C:\Program Files (x86)\JetBrains\PyCharm 4.5.4\bin\pycharm.exe"
